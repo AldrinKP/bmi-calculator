27ab9d4d1d5889f8e11d8bc327ca92673fc2f841
AldrinKP <aldrinpastores@gmail.com>
2024-08-05 13:24:36 +0200

    Created first text section

--- bmi-calculator/src/App.jsx
+++ bmi-calculator/src/App.jsx
@@ -1,6 +1,16 @@
 function App() {
 	return (
-		<h1 className="font-inter text-3xl font-semibold">BMI Calculator</h1>
+		<div className="w-[564px]">
+			<h1 className="font-inter text-3xl font-bold text-gunmetal leading-heading tracking-[-0.075em] mb-[35px]">
+				Body Mass Index Calculator
+			</h1>
+			<p className="text-deb leading-normal w-[465px]">
+				Better understand your weight in relation to your height using
+				our body mass index (BM) calculator. While BMI is not the sole
+				determinant of a healthy weight, it offers a valuable starting
+				point to evaluate your overall health and well-being.
+			</p>
+		</div>
 	);
 }
 
