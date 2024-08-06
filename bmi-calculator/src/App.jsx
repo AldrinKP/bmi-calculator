5fae6a6e0a9a92d10689d2f5475a91b8a2df0123
AldrinKP <aldrinpastores@gmail.com>
2024-08-06 15:37:45 +0200

    Added TipsSection component

--- bmi-calculator/src/App.jsx
+++ bmi-calculator/src/App.jsx
@@ -1,11 +1,13 @@
 import CalculatorSection from './components/CalculatorSection';
 import ResultSection from './components/ResultSection';
+import TipsSection from './components/TipsSection';
 
 function App() {
 	return (
 		<div>
 			<CalculatorSection />
 			<ResultSection />
+			<TipsSection />
 		</div>
 	);
 }
