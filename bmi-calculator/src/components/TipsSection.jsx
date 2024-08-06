56ebf06e2d4f28ad59b69ca6e1df85b768a8aa03
AldrinKP <aldrinpastores@gmail.com>
2024-08-06 15:37:25 +0200

    Started Tips Section component

---
+++ bmi-calculator/src/components/TipsSection.jsx
@@ -0,0 +1,9 @@
+const TipsSection = () => {
+	return (
+		<div className="flex justify-center items-center my-[96px] mx-auto rounded-[35px] bg-gradient-to-tl from-[#D6E6FE]/25 from-0% w-[1392px] h-[402px]">
+			<div className="flex  border-2 w-[1159px] h-[234px]"></div>
+		</div>
+	);
+};
+
+export default TipsSection;
