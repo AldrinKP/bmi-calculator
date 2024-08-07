91f84ca5010c377c892ed6f24726dc9b4dc66ac0
AldrinKP <aldrinpastores@gmail.com>
2024-08-07 13:54:29 +0200

    Completed Tip Card component

--- bmi-calculator/src/components/TipCard.jsx
+++ bmi-calculator/src/components/TipCard.jsx
@@ -1,5 +1,10 @@
-const TipCard = ({ icon, className, children }) => {
-	return <div>TipCard</div>;
+const TipCard = ({ icon, children }) => {
+	return (
+		<div className="flex flex-col w-[365px]">
+			<div className="mb-[45px]">{icon}</div>
+			{children}
+		</div>
+	);
 };
 
 export default TipCard;
