2d0ff6cc6813e5874b679df27274d592e1356a27
AldrinKP <aldrinpastores@gmail.com>
2024-08-09 15:01:37 +0200

    Created state for imperial values and connected to imperial inputs

--- bmi-calculator/src/components/CalculatorSection.jsx
+++ bmi-calculator/src/components/CalculatorSection.jsx
@@ -11,6 +11,12 @@ const calculateBMI = (system, height, weight) => {
 
 const CalculatorSection = () => {
 	const [metricValues, setMetricValues] = useState({ height: 0, weight: 0 });
+	const [imperialValues, setImperialValues] = useState({
+		ft: 0,
+		in: 0,
+		st: 0,
+		lbs: 0,
+	});
 	const [radioValue, setRadioValue] = useState('metric');
 
 	const handleInputChange = (inputId, value) => {
@@ -78,76 +84,76 @@ const CalculatorSection = () => {
 		</div>
 	);
 
-	// const imperialInputFields = (
-	// 	<>
-	// 		<div className="flex gap-[60px]">
-	// 			<div className="flex flex-col">
-	// 				<label className="mb-2 text-sm font-inter text-deb">
-	// 					Height
-	// 				</label>
-	// 				<div>
-	// 					<input
-	// 						type="text"
-	// 						value={heightValue}
-	// 						onChange={(e) => {
-	// 							handleInputChange('height', e.target.value);
-	// 						}}
-	// 						className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
-	// 					></input>
-	// 					<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
-	// 						ft
-	// 					</span>
-	// 				</div>
-	// 			</div>
-	// 			<div className="content-end">
-	// 				<input
-	// 					type="text"
-	// 					value={weightValue}
-	// 					onChange={(e) => {
-	// 						handleInputChange('weight', e.target.value);
-	// 					}}
-	// 					className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
-	// 				></input>
-	// 				<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
-	// 					in
-	// 				</span>
-	// 			</div>
-	// 		</div>
-	// 		<div className="flex gap-[57px]">
-	// 			<div className="flex flex-col">
-	// 				<label className="mb-2 text-sm font-inter text-deb">
-	// 					Height
-	// 				</label>
-	// 				<div>
-	// 					<input
-	// 						type="text"
-	// 						value={heightValue}
-	// 						onChange={(e) => {
-	// 							handleInputChange('height', e.target.value);
-	// 						}}
-	// 						className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
-	// 					></input>
-	// 					<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
-	// 						st
-	// 					</span>
-	// 				</div>
-	// 			</div>
-	// 			<div className="content-end">
-	// 				<input
-	// 					type="text"
-	// 					value={weightValue}
-	// 					onChange={(e) => {
-	// 						handleInputChange('weight', e.target.value);
-	// 					}}
-	// 					className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
-	// 				></input>
-	// 				<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
-	// 					lbs
-	// 				</span>
-	// 			</div>
-	// 		</div>
-	// 	</>
-	// );
+	const imperialInputFields = (
+		<>
+			<div className="flex gap-[60px]">
+				<div className="flex flex-col">
+					<label className="mb-2 text-sm font-inter text-deb">
+						Height
+					</label>
+					<div>
+						<input
+							type="text"
+							value={imperialValues.ft}
+							onChange={(e) => {
+								handleInputChange('height', e.target.value);
+							}}
+							className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
+						></input>
+						<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
+							ft
+						</span>
+					</div>
+				</div>
+				<div className="content-end">
+					<input
+						type="text"
+						value={imperialValues.in}
+						onChange={(e) => {
+							handleInputChange('weight', e.target.value);
+						}}
+						className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
+					></input>
+					<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
+						in
+					</span>
+				</div>
+			</div>
+			<div className="flex gap-[57px]">
+				<div className="flex flex-col">
+					<label className="mb-2 text-sm font-inter text-deb">
+						Weight
+					</label>
+					<div>
+						<input
+							type="text"
+							value={imperialValues.st}
+							onChange={(e) => {
+								handleInputChange('height', e.target.value);
+							}}
+							className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
+						></input>
+						<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
+							st
+						</span>
+					</div>
+				</div>
+				<div className="content-end">
+					<input
+						type="text"
+						value={imperialValues.lbs}
+						onChange={(e) => {
+							handleInputChange('weight', e.target.value);
+						}}
+						className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold w-[238px] border-2 rounded-xl border-deb cursor-pointer"
+					></input>
+					<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
+						lbs
+					</span>
+				</div>
+			</div>
+		</>
+	);
 
 	if (metricValues.height > 0 && metricValues.weight > 0) {
 		const BMI = calculateBMI(
