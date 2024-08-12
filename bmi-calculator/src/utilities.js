7cc77a443b4592c0e84730b51cfdcdac94512dbb
AldrinKP <aldrinpastores@gmail.com>
2024-08-12 13:26:17 +0200

    Changed function to only handle metric system

--- bmi-calculator/src/utilities.js
+++ bmi-calculator/src/utilities.js
@@ -1,5 +1,3 @@
-export const calculateMetricBMI = (system, height, weight) => {
-	if (system === 'metric') {
-		return Math.round((weight / (height * height)) * 100000) / 10;
-	}
+export const calculateMetricBMI = (height, weight) => {
+	return Math.round((weight / (height * height)) * 100000) / 10;
 };
