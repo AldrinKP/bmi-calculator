9e87383ae5e3a0bbe9801f94d96954979c3d7b8e
AldrinKP <aldrinpastores@gmail.com>
2024-08-12 13:53:15 +0200

    Bug fix - missing function arrow

--- bmi-calculator/src/utilities.js
+++ bmi-calculator/src/utilities.js
@@ -1,3 +1,12 @@
 export const calculateMetricBMI = (height, weight) => {
 	return Math.round((weight / (height * height)) * 100000) / 10;
 };
+
+export const calculateImperialBMI = (ft, inches, st, lbs) => {
+	const heightInInches = ft * 12 + inches;
+	const weightInPounds = st * 14 + lbs;
+	return (
+		Math.round((weightInPounds / heightInInches / heightInInches) * 7030) /
+		10
+	);
+};
