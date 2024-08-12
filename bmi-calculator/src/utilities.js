dc35fe82055a9f44490e7edb4f26b5325df6c1e6
AldrinKP <aldrinpastores@gmail.com>
2024-08-12 14:04:45 +0200

    Updated functions to use toFixed(1)

--- bmi-calculator/src/utilities.js
+++ bmi-calculator/src/utilities.js
@@ -1,12 +1,13 @@
 export const calculateMetricBMI = (height, weight) => {
-	return Math.round((weight / (height * height)) * 100000) / 10;
+	const result = Math.round((weight / (height * height)) * 100000) / 10;
+	return result.toFixed(1);
 };
 
 export const calculateImperialBMI = (ft, inches, st, lbs) => {
 	const heightInInches = parseInt(ft) * 12 + parseInt(inches);
 	const weightInPounds = parseInt(st) * 14 + parseInt(lbs);
-	return (
+	const result =
 		Math.round((weightInPounds / heightInInches / heightInInches) * 7030) /
-		10
-	);
+		10;
+	return result.toFixed(1);
 };
