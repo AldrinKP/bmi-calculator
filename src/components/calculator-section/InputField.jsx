f5108aa7ef649757f6bf1ddf2eaa603453ec8e18
AldrinKP <aldrinpastores@gmail.com>
2024-08-14 15:57:58 +0200

    Added prop to handle smaller width

--- src/components/calculator-section/InputField.jsx
+++ src/components/calculator-section/InputField.jsx
@@ -1,11 +1,13 @@
-const InputField = ({ value, onChange, unit, className }) => {
+const InputField = ({ value, onChange, unit, width, className }) => {
 	return (
 		<div className={`${className ? className : ''}`}>
 			<input
 				type="text"
 				value={value}
 				onChange={onChange}
-				className="px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold lg:w-[238px] md:w-[300px] border-2 rounded-xl border-deb cursor-pointer"
+				className={`px-6 py-5 text-gunmetal text-heading-m font-inter font-semibold lg:w-[238px] md:w-[300px] ${
+					width ? width : 'w-[280px]'
+				} border-2 rounded-xl border-deb cursor-pointer`}
 			></input>
 			<span className="ml-[-55px] text-blue font-inter text-heading-m font-semibold">
 				{unit}
