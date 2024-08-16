021a645e7541fbc7122f5898547d63ef050a3c6a
AldrinKP <aldrinpastores@gmail.com>
2024-08-16 13:34:28 +0200

    Completed support for tablet and mobile devices

--- src/components/LimitSection.jsx
+++ src/components/LimitSection.jsx
@@ -5,7 +5,7 @@ import { limitations } from '../assets/content';
 
 const LimitSection = () => {
 	return (
-		<div className="relative my-[120px] lg:w-[1160px] lg:h-[704px] mx-4">
+		<div className="relative lg:block flex flex-col items-center gap-[56px] my-[120px] lg:w-[1160px] lg:h-[704px] mx-6">
 			<TextGroup className="lg:gap-[35px] gap-8 lg:w-[564px] w-auto lg:text-start text-center lg:mx-0 mx-2">
 				<Heading className="tracking-tight lg:text-heading-l text-[32px] font-semibold">
 					Limitations of BMI
@@ -31,61 +31,64 @@ const LimitSection = () => {
 					/>
 				</svg>
 			</div>
-			<div className="absolute top-0 right-[99px] w-[365px] rounded-2xl shadow-custom-shadow bg-white">
-				<TextGroup className="p-8 gap-4">
-					<div className="flex gap-3 items-center">
-						{limitations.gender.icon}
-						<Heading className="text-heading-s font-semibold">
-							{limitations.gender.title}
-						</Heading>
+			<div className="lg:block md:grid md:grid-cols-2 md:gap-y-6 md:gap-x-4 flex flex-col gap-4">
+				<div className="lg:absolute static top-0 right-[99px] lg:w-[365px] rounded-2xl shadow-custom-shadow bg-white">
+					<TextGroup className="p-8 gap-4">
+						<div className="flex gap-3 items-center">
+							{limitations.gender.icon}
+							<Heading className="text-heading-s font-semibold">
+								{limitations.gender.title}
+							</Heading>
+						</div>
+						<BodyText>{limitations.gender.text}</BodyText>
+					</TextGroup>
+				</div>
+				<div className="lg:absolute static top-[264px] right-[397px] lg:w-[365px] rounded-2xl shadow-custom-shadow bg-white">
+					<TextGroup className="p-8 gap-4">
+						<div className="flex gap-3 items-center">
+							{limitations.age.icon}
+							<Heading className="text-heading-s font-semibold">
+								{limitations.age.title}
+							</Heading>
+						</div>
+						<BodyText>{limitations.age.text}</BodyText>
+					</TextGroup>
+				</div>
+				<div className="lg:absolute static top-[264px] right-0 lg:w-[365px] rounded-2xl shadow-custom-shadow bg-white">
+					<TextGroup className="p-8 gap-4">
+						<div className="flex gap-3 items-center">
+							{limitations.muscle.icon}
+							<Heading className="text-heading-s font-semibold">
+								{limitations.muscle.title}
+							</Heading>
+						</div>
+						<BodyText>{limitations.muscle.text}</BodyText>
+					</TextGroup>
+				</div>
+				<div className="lg:absolute static bottom-0 right-[199px] lg:w-[365px] rounded-2xl shadow-custom-shadow bg-white">
+					<TextGroup className="p-8 gap-4">
+						<div className="flex gap-3 items-center">
+							{limitations.pregnancy.icon}
+							<Heading className="text-heading-s font-semibold">
+								{limitations.pregnancy.title}
+							</Heading>
+						</div>
+						<BodyText>{limitations.pregnancy.text}</BodyText>
+					</TextGroup>
+				</div>
+				<div className="lg:block md:col-span-2 md:flex md:justify-center">
+					<div className="lg:absolute static bottom-0 left-[199px] lg:w-[365px] md:w-1/2 rounded-2xl shadow-custom-shadow bg-white">
+						<TextGroup className="p-8 gap-4">
+							<div className="flex gap-3 items-center">
+								{limitations.race.icon}
+								<Heading className="text-heading-s font-semibold">
+									{limitations.race.title}
+								</Heading>
+							</div>
+							<BodyText>{limitations.race.text}</BodyText>
+						</TextGroup>
 					</div>
-					<BodyText>{limitations.gender.text}</BodyText>
-				</TextGroup>
-			</div>
-			<div className="absolute top-[264px] right-[397px] w-[365px] rounded-2xl shadow-custom-shadow bg-white">
-				<TextGroup className="p-8 gap-4">
-					<div className="flex gap-3 items-center">
-						{limitations.age.icon}
-						<Heading className="text-heading-s font-semibold">
-							{limitations.age.title}
-						</Heading>
-					</div>
-					<BodyText>{limitations.age.text}</BodyText>
-				</TextGroup>
-			</div>
-			<div className="absolute top-[264px] right-0 w-[365px] rounded-2xl shadow-custom-shadow bg-white">
-				<TextGroup className="p-8 gap-4">
-					<div className="flex gap-3 items-center">
-						{limitations.muscle.icon}
-						<Heading className="text-heading-s font-semibold">
-							{limitations.muscle.title}
-						</Heading>
-					</div>
-					<BodyText>{limitations.muscle.text}</BodyText>
-				</TextGroup>
-			</div>
-
-			<div className="absolute bottom-0 right-[199px] w-[365px] rounded-2xl shadow-custom-shadow bg-white">
-				<TextGroup className="p-8 gap-4">
-					<div className="flex gap-3 items-center">
-						{limitations.pregnancy.icon}
-						<Heading className="text-heading-s font-semibold">
-							{limitations.pregnancy.title}
-						</Heading>
-					</div>
-					<BodyText>{limitations.pregnancy.text}</BodyText>
-				</TextGroup>
-			</div>
-			<div className="absolute bottom-0 left-[199px] w-[365px] rounded-2xl shadow-custom-shadow bg-white">
-				<TextGroup className="p-8 gap-4">
-					<div className="flex gap-3 items-center">
-						{limitations.race.icon}
-						<Heading className="text-heading-s font-semibold">
-							{limitations.race.title}
-						</Heading>
-					</div>
-					<BodyText>{limitations.race.text}</BodyText>
-				</TextGroup>
+				</div>
 			</div>
 		</div>
 	);
